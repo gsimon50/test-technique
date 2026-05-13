72c2aa6862a6419ac69b93274b9f6f76d47e0dcb
Geoffrey <geoffrey@Mac.lan>
2026-05-13 12:26:54 +0200

    mise en place de la connexion

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -17,16 +17,72 @@ final class AuthController extends AbstractController
         private Connection $connection
     ) {}
     
-    #[Route('/login', name: 'app_auth')]
-    public function index(): Response
-    {
-        return $this->render('auth/index.html.twig', [
-            'login' => true,
-            'error' => '',
+    ////// Accès a la page de connexion //////
+        
+        #[Route('/login', name: 'app_login')]
+        public function index(): Response
+        {
+            return $this->render('auth/index.html.twig', [
+                'login' => true,
+                'mail' => null,
+                'error' => '',
+            ]);
+        }
 
-        ]);
-    }
+    ////// //////
 
+    ////// Connexion //////
+
+        #[Route('/login', name: 'app_login_submit', methods: ['POST'])]
+        public function connectUser(Request $request): Response {
+            $mail   = $request->request->get('email');
+            $psw    = $request->request->get('password');
+
+            try {
+                $this->checkConnexion($mail, $psw);
+            } catch (\InvalidArgumentException $e){
+                return $this->render('auth/index.html.twig', [
+                    'login' => true,
+                    'mail' => $mail,
+                    'error' => $e->getMessage(),
+                ]);
+            }
+
+            return $this->redirectToRoute('app_home');
+        }
+
+
+    ////// //////
+
+    ////// checkUser //////
+
+        private function checkConnexion(string $mail,string $psw){
+
+            if(!$this->getEmailPassword($mail, $psw)){
+                throw new \InvalidArgumentException('Le mail ou le mot de passe ne correspond pas.');
+            }
+        }
+
+
+    ////// //////
+
+    ////// getEmailPassword //////
+
+        private function getEmailPassword(string $mail,string $psw){
+            $result = $this->connection->executeQuery(
+                'SELECT * FROM user WHERE email = :email;',
+                ['email' => $mail]
+            )->fetchAssociative();
+
+            if (!$result) {
+                return false;
+            }
+
+            return password_verify($psw, $result['password']);
+        }
+
+
+    ////// //////
 
     ////// Accès a la page de registration //////
         #[Route('/register', name: 'app_register', methods: ['GET'])]
@@ -34,6 +90,10 @@ final class AuthController extends AbstractController
         {
             return $this->render('auth/index.html.twig', [
                 'login' => false,
+                'nom' => null,
+                'prenom' => null,
+                'mail' => null,
+                'mail_check' => null,
                 'error' => '',
             ]);
         }
@@ -43,19 +103,23 @@ final class AuthController extends AbstractController
 
         #[Route('/register', name: 'app_register_submit', methods: ['POST'])]
         public function createUser(Request $request): Response {
-            $nom      = $request->request->get('nom');
-            $prenom   = $request->request->get('prenom');
-            $mail    = $request->request->get('email');
-            $mail_check    = $request->request->get('email_confirm');
-            $psw = $request->request->get('password');
-            $psw_check = $request->request->get('password_confirm');
+            $nom        = $request->request->get('nom');
+            $prenom     = $request->request->get('prenom');
+            $mail       = $request->request->get('email');
+            $mail_check = $request->request->get('email_confirm');
+            $psw        = $request->request->get('password');
+            $psw_check  = $request->request->get('password_confirm');
 
             try {
-                $password = $this->checkpsw($psw, $psw_check);
-                $email = $this->checkmail($mail, $mail_check);
+                $password   = $this->checkpsw($psw, $psw_check);
+                $email      = $this->checkmail($mail, $mail_check);
             } catch (\InvalidArgumentException $e) {
                 return $this->render('auth/index.html.twig', [
                     'login' => false,
+                    'nom' => $nom,
+                    'prenom' => $prenom,
+                    'mail' => $mail,
+                    'mail_check' => $mail_check,
                     'error' => $e->getMessage(),
                 ]);
             }
@@ -101,16 +165,23 @@ final class AuthController extends AbstractController
                 throw new \InvalidArgumentException('Les adresses e-mail ne correspondent pas.');
             }
 
-            $result = $this->connection->executeQuery(
-                'SELECT * FROM user WHERE email = :email',
-                ['email' => $mail]
-            )->fetchAssociative();
-
-            if ($result) {
+            if ($this->getEmail($mail)) {
                 throw new \InvalidArgumentException('Email déjà existant.');
             }
 
             return $mail;
         }
     ////// //////
+
+    ////// Get Email //////
+        private function getEmail(string $mail){
+            $result = $this->connection->executeQuery(
+                'SELECT * FROM user WHERE email = :email',
+                ['email' => $mail]
+            )->fetchAssociative();
+
+            return $result;
+        }
+    ////// //////
+
 }
